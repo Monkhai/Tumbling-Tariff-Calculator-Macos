416318efe48609f838485e6d7f99281f63bbf769
Yohai Wiener <yohaiwiener@gmail.com>
2023-12-24 11:12:15 +0200

    Initial Commit

---
+++ Tumbling Tariff Calculator/Tumbling_Tariff_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tumbling_Tariff_CalculatorApp.swift
+//  Tumbling Tariff Calculator
+//
+//  Created by Yohai Wiener on 2023-12-24.
+//
+
+import SwiftUI
+
+@main
+struct Tumbling_Tariff_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
